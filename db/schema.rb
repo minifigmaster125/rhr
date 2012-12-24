8fd1bfc4fb5caddbae1ec84d53fa22e2a6e491ff
Suchaaver S. Chahal <xchaoscontrolx@gmail.com>
2012-12-24 14:54:44 -0800

    made posts clickable

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121221194919) do
+ActiveRecord::Schema.define(:version => 20121224210556) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -65,6 +65,14 @@ ActiveRecord::Schema.define(:version => 20121221194919) do
     t.string   "name"
     t.text     "bio"
     t.integer  "year_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "posts", :force => true do |t|
+    t.string   "title"
+    t.date     "date"
+    t.text     "entry"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
