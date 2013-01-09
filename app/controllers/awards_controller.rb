68e40e8f5593897c2b3234bd1d323179702da031
Suchaaver S. Chahal <xchaoscontrolx@gmail.com>
2013-01-08 17:29:43 -0800

    ready to push live

--- app/controllers/awards_controller.rb
+++ app/controllers/awards_controller.rb
@@ -1,17 +1,9 @@
 class AwardsController < ApplicationController
 
     def new
-        @award = Award.new
     end
 
     def create
-        @year = Year.find(params[:year_id]
-        @award = @year.awards.new(params[:award])
-        if  @award.save	
-            redirect to year_show_path
-        else
-            render "new"
-        end 
     end
 
 end
