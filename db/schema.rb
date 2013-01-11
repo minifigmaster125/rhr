f09496d5a040888325352738a0df724210972861
Joraaver Chahal <joraaverchahal@gmail.com>
2013-01-10 19:29:13 -0800

    pulling from Suchaaver

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-
 # encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
@@ -87,4 +86,3 @@ ActiveRecord::Schema.define(:version => 20130110012458) do
   end
 
 end
-
